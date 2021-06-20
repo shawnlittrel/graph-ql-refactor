3cd954c28e2e302097825710121ce742068569a5
Shawn Littrel <shawnlittrel@protonmail.com>
2021-06-19 17:28:57 -0700

    backend queries working

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,85 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { Book, User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+//what api calls do we need to make?
+// createUser -> mutation  -> DONE
+// login -> mutation  -> DONE
+// saveBook -> mutation
+// deleteBook -> mutation
+// getSingleUser -> query
+// getMe -> query
+
+const resolvers = {
+     Query:{
+          me: async (parent, args, context) => {
+               if(context.user) {
+                    const userData = await User.findOne({})
+                    .select('-__v -password')
+                    .populate('savedBooks');
+
+                    return userData;
+               }
+               throw new AuthenticationError('Not logged in');
+          },
+     },
+     Mutation:{
+          addUser: async (parent, args) => {
+               const user = await User.create(args);
+               const token = signToken(user);
+
+               return { token, user };
+          },
+
+          login: async (parent, { email, password }) => {
+               const user = await User.findOne({ email });
+
+               if (!user) {
+                    throw new AuthenticationError('Incorrect Credentials');
+               }
+
+               const correctPw = await user.isCorrectPassword(password);
+
+               if (!correctPw) {
+                    throw new AuthenticationError('Incorrect Credentials');
+               }
+
+               const token = signToken(user);
+
+               return { token, user };
+          },
+
+          saveBook: async (parent, { bookId }, context) => {
+               if (context.user) {
+                    const updatedUser = await User.findOneAndUpdate(
+                         { _id: user._id },
+                         { $addToSet: { savedBooks: bookId } },
+                         { new: true, runValidators: true }
+                    ).populate('savedBooks');
+
+                    return updatedUser;
+               }
+
+               throw new AuthenticationError('You need to be logged in');
+          },
+
+          removeBook: async (parent, { bookId }, context) => {
+               if (context.user) {
+                    const updatedUser = await User.findOneAndUpdate(
+                         { _id: user._id },
+                         { $pull: { savedBooks: bookId } },
+                         { new: true }
+                    )
+
+                    return updatedUser;
+               }
+
+               throw new AuthenticationError('You need to be logged in');
+          },
+
+
+
+     }
+};
+
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+
+//typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+//integrate Apollo server
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +27,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//serve index regardless of what path is requested
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+
+  //log graphQL location
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
